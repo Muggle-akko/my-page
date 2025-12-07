6d14a1a7f4391b0561e1fe461b29e596f0da4f54
MOO GAI CHUNG <272225660@qq.com>
2025-12-07 16:12:30 +0800

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Quartz 4",
+    pageTitle: "阿巫的数字花园",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
