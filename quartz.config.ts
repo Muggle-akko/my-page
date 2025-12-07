3d24d4d0330e6cc426f51d4a7c9ffae56f5a9b8c
MOO GAI CHUNG <272225660@qq.com>
2025-12-07 18:45:50 +0800

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "zh-CN",
-    baseUrl: "my-page",
+    baseUrl: "muggle-akko.github.io/my-page",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
