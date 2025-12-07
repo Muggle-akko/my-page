7349cfccda0a3a03f2eb3c57c3c32c935cafdd08
MOO GAI CHUNG <272225660@qq.com>
2025-12-07 16:26:01 +0800

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -15,7 +15,16 @@ const config: QuartzConfig = {
     analytics: {
       provider: "plausible",
     },
-    locale: "en-US",
+    locale: "zh-CN",
+    text: {
+      'Search': '搜索',
+      'Explorer': '文件浏览器',
+      'Table of Contents': '目录',
+      'Backlinks': '反向链接',
+      'Recent Files': '最近文件',
+      'No search results for': '没有找到关于',
+      'in this file': '在这个文件中',
+    },
     baseUrl: "quartz.jzhao.xyz",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
